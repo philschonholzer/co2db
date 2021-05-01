43f35935bc4e5cca1d9b402abedad2fc409f8c33
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2021-05-01 10:42:47 +0200

    Added create user table migration

---
+++ Application/Migration/1619856912-create-user-table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    email TEXT NOT NULL,
+    password_hash TEXT NOT NULL,
+    locked_at TIMESTAMP WITH TIME ZONE DEFAULT NULL,
+    failed_login_attempts INT DEFAULT 0 NOT NULL
+);
