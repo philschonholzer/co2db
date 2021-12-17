d1e43142606cb80a855d9e9d40647e56b744274a
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2021-12-17 19:16:42 +0100

    Added QuickChecks

--- Test/Helper/AverageSpec.hs
+++ Test/Helper/AverageSpec.hs
@@ -1,10 +1,9 @@
 module Test.Helper.AverageSpec where
 
 import Application.Helper.Average (average)
-import IHP.FrameworkConfig
-import IHP.HaskellSupport
 import IHP.Prelude
 import Test.Hspec
+import Test.Hspec.QuickCheck
 
 tests :: Spec
 tests = describe "Average" $ do
@@ -15,7 +14,21 @@ tests = describe "Average" $ do
     average [1.0, 12.0] `shouldBe` Just 6.5
     average [1.5, 2.5] `shouldBe` Just 2
     average [1.0] `shouldBe` Just 1
+    average [4.1] `shouldBe` Just 4.1
+    average [0.0] `shouldBe` Just 0.0
 
   it "does not calculate average of bad inputs" $ do
     average [] `shouldBe` Nothing
     average Nothing `shouldBe` Nothing
+
+  prop "largest number in the list is higher-equal than the average" $ \(x :: [Double]) ->
+    average x
+      `shouldSatisfy` \case
+        Nothing -> null x
+        Just result -> result <= maximum x
+
+  prop "smallest number in the list is smaller-equal than the average" $ \(x :: [Double]) ->
+    average x
+      `shouldSatisfy` \case
+        Nothing -> null x
+        Just result -> result >= minimum x
